82312cd8b10e14286d1e6efb05309e959c41543c
rodrigo brandão <rodrigo.brandao@spotmetrics.com>
2023-05-15 04:18:14 -0300

    a

--- app/routes/nested/internal.tsx
+++ app/routes/nested/internal.tsx
@@ -1,39 +1,79 @@
+import { css } from '@emotion/css';
 import type { ActionArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { Form, useActionData, useLoaderData } from '@remix-run/react';
 
+const formDataToJson = (data: FormData) => {
+    const keys = [...new Set(Array.from(data.keys()))];
+    const object = Object.fromEntries(
+        keys.map((key) => [
+            key,
+            data.getAll(key).length > 1 ? data.getAll(key) : data.get(key),
+        ])
+    );
+    return JSON.parse(JSON.stringify(object));
+};
+
 export const loader = async () => {
-    return json({ name: 'Zelda' });
+    const data = {
+        game: 'The Legend Of Zelda',
+        title: 'Tears Of The Kingdom',
+    };
+    return json(data);
 };
 
 export const action = async ({ request }: ActionArgs) => {
     const formData = await request.formData();
-    const obj = Object.fromEntries(
-        Array.from(formData.keys()).map((key) => [
-            key,
-            formData.getAll(key).length > 1
-                ? formData.getAll(key)
-                : formData.get(key),
-        ])
-    );
-    console.log(obj);
-    return json(obj);
+    const data = formDataToJson(formData);
+    return json(data);
 };
 
-export default function MyForm() {
-    const loader = useLoaderData();
-    const action = useActionData();
+export default function Component() {
+    const loaderData = useLoaderData<typeof loader>();
+    const actionData = useActionData<typeof action>();
 
     return (
-        <Form method="post">
-            <p>{JSON.stringify(action)}</p>
-            <input
-                defaultValue={loader.name}
-                id="name"
-                name="name"
-                title="Name"
-            />
-            <button type="submit">Submit Form</button>
-        </Form>
+        <div
+            className={css`
+                label {
+                    display: block;
+                    font-size: 12px;
+                    color: #888;
+                    margin: 0 0 20px 0;
+
+                    input {
+                        display: block;
+                    }
+                }
+            `}
+        >
+            <Form action="/nested/internal" method="post">
+                <p>{JSON.stringify(actionData)}</p>
+                <p>{JSON.stringify(loaderData)}</p>
+                <label>
+                    Game
+                    <input
+                        defaultValue={loaderData.game}
+                        name="game"
+                        type="text"
+                    />
+                </label>
+                <label>
+                    Title
+                    <input
+                        defaultValue={loaderData.title}
+                        name="title"
+                        type="text"
+                    />
+                </label>
+                <label>
+                    Cover
+                    <input name="cover" type="file" />
+                </label>
+                <label>
+                    <button type="submit">Submit</button>
+                </label>
+            </Form>
+        </div>
     );
 }
